e2549229adef5d9fd007438c9142f5b84e08c8fd
DarthGonk <DarthGonk@DESKTOP-5BGEPG8>
2022-09-08 02:40:11 -0700

    Add project files.

---
+++ RollerCoasterOperatorSimulator/Model/Rockets.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollerCoasterOperatorSimulator.Model
+{
+    public class Rockets
+    {
+        List<Rocket> rockets; //List of all our ROCKETS
+        Zones zones;
+        /// <summary>
+        /// Constructor which is instanitated
+        /// </summary>
+        public Rockets(Zones zones)
+        {
+            rockets = new List<Rocket>(); 
+            this.zones = zones;
+            Intialize();
+        }
+
+        /// <summary>
+        /// Intialize creates all the zones so we have places for the rockets.
+        /// </summary>
+        private void Intialize()
+        {
+            rockets.Add(new Rocket(1, zones.GetByName("Dispatch"),false));
+            rockets.Add(new Rocket(2, zones.GetByName("Ready"), false));
+            rockets.Add(new Rocket(3, zones.GetByName("Load"), false));
+            rockets.Add(new Rocket(4, zones.GetByName("Unload"), false));
+            rockets.Add(new Rocket(5, zones.GetByName("Hold 1"), false)); ;
+            rockets.Add(new Rocket(6, zones.GetByName("Hold 2"), false));
+
+            for (int i = 1; i <= 12; i++)
+            {
+                rockets.Add(new Rocket(i, zones.GetByName("Storage"), true));
+            }
+        }
+    }
+    
+    public class Rocket
+    {
+        public int Id { get; set; }
+
+        public bool inStorage { get; set; }
+
+        public Zone OccupiedZone { get; set; }
+
+        public Rocket(int Id, Zone OccupiedZone, bool inStorage = false)
+        {
+            this.Id = Id;
+            this.inStorage = inStorage;
+        }
+    }
+}
---
+++ RollerCoasterOperatorSimulator/Model/Zones.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RollerCoasterOperatorSimulator.Model
+{
+    public class Zones
+    {
+        List<Zone> zones; //List of all our zones
+
+        /// <summary>
+        /// Constructor which is instanitated
+        /// </summary>
+        public Zones()
+        {
+            zones = new List<Zone>();
+            Intialize();
+        }
+
+        /// <summary>
+        /// Intialize creates all the zones so we have places for the rockets.
+        /// </summary>
+        private void Intialize()
+        {
+            for(int i = 1; i <= 15; i++)
+            {
+                zones.Add(new Zone($"Zone {i}", i, false));        
+            }
+            zones.Add(new Zone($"Hold 2", 16, true));
+            zones.Add(new Zone($"Hold 1", 17, true));
+            zones.Add(new Zone($"Unload", 18, true));
+            zones.Add(new Zone($"Load", 19, true));
+            zones.Add(new Zone($"Ready", 20, true));
+            zones.Add(new Zone($"Dispatch", 21, true));
+            zones.Add(new Zone($"Storage", 22, true));
+        }
+
+        /// <summary>
+        /// Gets the Zone by the index number
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns>Desired zone at specific index</returns>
+        public Zone GetByIndex(int index)
+        {
+            return zones[index];
+        }
+
+        /// <summary>
+        /// Get's a zone by the zone name, throws exception is the zone is not found
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>Zone</returns>
+        /// <exception cref="Exception"></exception>
+        public Zone GetByName(string name)
+        {
+            foreach(Zone zone in zones)
+            {
+                if(zone.Name.Equals(name))
+                    return zone;
+            }
+            throw new Exception("Zone not found.");
+        }
+
+        /// <summary>
+        /// Get's a zone by the zone id. Throws exception if zone is not found
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
+        public Zone GetByID(int id)
+        {
+            foreach (Zone zone in zones)
+            {
+                if (zone.Id.Equals(id))
+                    return zone;
+            }
+            throw new Exception("Zone not found.");
+        }
+    }
+
+    /// <summary>
+    /// Zone class
+    /// </summary>
+    public class Zone
+    {
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+
+        public bool Occupied { get; set; }
+
+        public Zone(string Name, int Id, bool Occupied = false)
+        {
+            this.Name = Name;
+            this.Id = Id;
+            this.Occupied = Occupied;
+        }
+    }
+}
